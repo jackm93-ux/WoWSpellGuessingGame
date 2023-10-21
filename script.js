70a1ce7e01aafa091e8d47fabb23cd33d230cc38
JackMFirefly <jackm@fireflyworlds.com>
2023-10-21 15:33:22 +0100

    Added some basic scording and gameplay

--- script.js
+++ script.js
@@ -3,29 +3,146 @@
 const spellIcon = document.getElementById('spellIcon');
 const timer = document.querySelector('.timer');
 
+const answerButtons = document.querySelectorAll('.btn--answer');
+
+const currentScoreText = document.querySelector('.score--currentScore');
+const currentComboText = document.querySelector('.score--currentCombo');
+
 let timeInSeconds = 10;
 let timeLimit = 10;
+
+let currentScore = 0;
+let currentCombo = 1;
+
+const scorePerCorrectAnswer = 10;
 
 //measured in px
 let blurMax = 15;
 let currentBlur = 15;
 let blurDelta = 3.5;
 
-const iconCodes = [
-  'spell_nature_chainlightning',
-  'spell_holy_purify',
-  'ability_criticalstrike',
-  'spell_holy_powerwordshield',
-  'spell_frost_freezingbreath',
-  'spell_nature_lightning',
-  'ability_shockwave',
-  'spell_fire_fireball02',
-  'spell_shadow_possession',
-  'spell_fire_incinerate',
+const spells = [
+  {
+    iconCode: 'spell_nature_chainlightning',
+    answers: [
+      'Chain Lightning',
+      'Lightning bolt',
+      'Healing Wave',
+      'Thunderclap',
+    ],
+    correctAnswer: 'Chain Lightning',
+  },
+
+  {
+    iconCode: 'spell_holy_purify',
+    answers: ['Inner Fire', 'Flash heal', 'Power Word: Shield', 'Holy Light'],
+    correctAnswer: 'Power Word: Shield',
+  },
+
+  {
+    iconCode: 'ability_criticalstrike',
+    answers: ['Rend', 'Defensive stance', 'Shield Wall', 'Recklessness'],
+    correctAnswer: 'Recklessness',
+  },
+
+  {
+    iconCode: 'spell_holy_powerwordshield',
+    answers: [
+      'Chain Lightning',
+      'Lightning bolt',
+      'Power word: Shield',
+      'Thunderclap',
+    ],
+    correctAnswer: 'Power word: Shield',
+  },
+
+  {
+    iconCode: 'spell_frost_freezingbreath',
+    answers: ['Frost bolt', 'Frost Armour', 'Frost Nova', 'Ice Shield'],
+    correctAnswer: 'Frost Nova',
+  },
+
+  {
+    iconCode: 'spell_nature_lightning',
+    answers: [
+      'Chain Lightning',
+      'Lightning bolt',
+      'Healing Wave',
+      'Thunderclap',
+    ],
+    correctAnswer: 'Lightning bolt',
+  },
+
+  {
+    iconCode: 'spell_fire_fireball02',
+    answers: ['Fireball', 'Incinerate', 'Hellfire', 'Fireblast'],
+    correctAnswer: 'Fireball',
+  },
+
+  {
+    iconCode: 'ability_shockwave',
+    answers: ['Heroic strike', 'Rend', 'Deep wounds', 'Hamstring'],
+    correctAnswer: 'Hamstring',
+  },
+
+  {
+    iconCode: 'spell_shadow_possession',
+    answers: ['Mind Flay', 'Fear', 'Shadowbolt', 'Life Leech'],
+    correctAnswer: 'Fear',
+  },
+
+  {
+    iconCode: 'spell_fire_incinerate',
+    answers: ['Fireball', 'Incinerate', 'Hellfire', 'Fireblast'],
+    correctAnswer: 'Incinerate',
+  },
 ];
+
+let selectedSpell = spells[0];
+
+const selectSpell = function (spellIndex) {
+  selectedSpell = spells[spellIndex];
+};
 
 const getWoWImage = function (iconCode) {
   return `https://wow.zamimg.com/images/wow/icons/large/${iconCode}.jpg`;
+};
+
+const calculateScore = function () {
+  return Math.trunc(scorePerCorrectAnswer * currentBlur * currentCombo);
+};
+
+const updateScore = function (scoringFunction) {
+  currentScore += scoringFunction();
+  currentCombo++;
+
+  currentScoreText.textContent = `Score: ${currentScore}`;
+  currentComboText.textContent = `Combo: ${currentCombo}`;
+};
+
+const setUpAnswerButtons = function () {
+  for (let i = 0; i < answerButtons.length; i++) {
+    answerButtons[i].addEventListener('click', function checkAnswer() {
+      //we get a correct answer we update the score and combo
+      if (answerButtons[i].textContent === selectedSpell.correctAnswer) {
+        updateScore(calculateScore);
+        resetImageAndTimer();
+      }
+    });
+  }
+};
+
+const init = function () {
+  selectSpell(Math.trunc(Math.random() * spells.length));
+  spellIcon.src = getWoWImage(selectedSpell.iconCode);
+  setUpAnswerButtons();
+  populateAnswerButtons(selectedSpell.answers);
+};
+
+const populateAnswerButtons = function (answers) {
+  for (let i = 0; i < answers.length; i++) {
+    answerButtons[i].textContent = answers[i];
+  }
 };
 
 const setCurrentBlurValue = function (blurValue) {
@@ -39,23 +156,25 @@ const setcurrentTimerValue = function (currentTime) {
 };
 
 const resetImageAndTimer = function () {
-  spellIcon.src = getWoWImage(
-    iconCodes[Math.trunc(Math.random() * iconCodes.length)]
-  );
-  setCurrentBlurValue(1);
+  selectSpell(Math.trunc(Math.random() * spells.length));
+  spellIcon.src = getWoWImage(selectedSpell.iconCode);
+  setCurrentBlurValue(blurMax);
   setcurrentTimerValue(timeLimit);
+  populateAnswerButtons(selectedSpell.answers);
 };
-
-spellIcon.src = getWoWImage(iconCodes[0]);
 
 //each second the interval is called and adjusts the timer and image blur based on the time. called every second and resets when the timer hits 0 choosing a new image
 const chooseNewIcon = setInterval(function () {
   if (timeInSeconds <= 0) {
     resetImageAndTimer();
+    currentCombo = 1;
+    currentComboText.textContent = currentCombo;
   }
   setcurrentTimerValue(timeInSeconds - 1);
   setCurrentBlurValue(currentBlur - blurMax / (timeInSeconds * blurDelta));
 }, 1000);
+
+init();
 
 //Commenting game logic here to make into problem breakdown
 //and flow chart later
